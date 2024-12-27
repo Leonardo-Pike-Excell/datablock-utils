4ad9cd332ff651992f090cb1c69e3a23dc257123
Leonardo-Pike-Excell <leonardopike.excell@gmail.com>
2024-12-27 17:47:46 +1100

    refactor: improve annotations for user_map.py

--- source/operators/user_map.py
+++ source/operators/user_map.py
@@ -1,13 +1,18 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+from typing import cast
 import bpy
 from bpy.props import IntProperty
-from bpy.types import ID, Context, Operator
+from bpy.types import ID, Context, EnumProperty, Operator
 
 from ..constants import ID_TYPES, get_id_type
-from ..properties import DBU_PG_ParentItem
+from ..properties import DBU_PG_ParentItem, DBU_PG_UserItem, DBU_PG_UserMapSettings
 
 _EXCLUDED_VALUE_TYPES = {'COLLECTION', 'WINDOWMANAGER', 'WORKSPACE'}
+
+
+def get_settings() -> DBU_PG_UserMapSettings:
+    return bpy.context.scene.dbu_users_settings  # type: ignore
 
 
 class DBU_OT_UserMap(Operator):
@@ -24,7 +29,7 @@ class DBU_OT_UserMap(Operator):
       precomputed: dict[ID, set[ID]],
       ancestors: set[ID],
     ) -> None:
-        settings = bpy.context.scene.dbu_users_settings
+        settings = get_settings()
         user_map = settings.user_map
 
         name = user.name
@@ -33,11 +38,11 @@ class DBU_OT_UserMap(Operator):
         if not settings.OBJECT and ID_TYPES[id_type].is_object_data:
             return
 
-        as_parent = user_map.add()
+        as_parent: DBU_PG_ParentItem = user_map.add()
         as_parent.name = name
         as_parent.id_type = id_type
 
-        as_user = parent.users.add()
+        as_user: DBU_PG_UserItem = parent.users.add()
         as_user.name = name
         as_user.id_type = id_type
         as_user.as_parent_idx = len(user_map) - 1
@@ -45,33 +50,29 @@ class DBU_OT_UserMap(Operator):
         if user in ancestors:
             return
 
-        ntree = getattr(user, 'node_tree', user)
-        try:
-            nodes = ntree.nodes
-        except AttributeError:
-            pass
-        else:
+        nodes: bpy.types.Nodes | None = getattr(getattr(user, 'node_tree', user), 'nodes', None)
+        if nodes is not None:
             # yapf: disable
             if parent.id_type != 'IMAGE':
                 node_names = [
-                  n.name for n in nodes if hasattr(n, 'node_tree')
-                  and n.node_tree and n.node_tree.name == parent.name]
+                  n.name for n in nodes if getattr(n, 'node_tree', None)
+                  and n.node_tree.name == parent.name] # type: ignore
             else:
                 node_names = [
                   n.name for n in nodes if hasattr(n, 'image')
-                  and n.image.name == parent.name]
+                  and n.image.name == parent.name] # type: ignore
             # yapf: enable
 
             for name in sorted(node_names):
-                n = as_user.node_names.add()
-                n.name = name
+                item = as_user.node_names.add()
+                item.name = name
 
         for u in precomputed[user]:
             if u != user:
                 cls.add_users(as_parent, u, precomputed, ancestors | {user})
 
     def execute(self, context: Context) -> set[str]:
-        settings = context.scene.dbu_users_settings
+        settings = get_settings()
         parents = [p for p in settings.parents if p.id_type != 'UNDEFINED']
 
         if not parents:
@@ -84,12 +85,14 @@ class DBU_OT_UserMap(Operator):
         setting_enums = {e for e in dir(settings) if e.isupper()}
         value_types = {e for e in setting_enums if getattr(settings, e)}
         if settings.others:
-            prop = bpy.types.KeyingSetPath.bl_rna.properties['id_type']
+            prop = cast(EnumProperty, bpy.types.KeyingSetPath.bl_rna.properties['id_type'])
             value_types.update(set(prop.enum_items.keys()) - setting_enums)
 
         value_types -= _EXCLUDED_VALUE_TYPES
-        key_types = value_types.union(settings.bl_rna.properties['id_type'].enum_items.keys())
-        precomputed = bpy.data.user_map(key_types=key_types, value_types=value_types)
+        prop = cast(EnumProperty, settings.bl_rna.properties['id_type'])
+        key_types = value_types.union(prop.enum_items.keys())
+        precomputed = bpy.data.user_map(
+          key_types=key_types, value_types=value_types)  # type: ignore
 
         for temp_parent in parents:
             name = temp_parent.name
@@ -113,7 +116,7 @@ class DBU_OT_UserMapClearResults(Operator):
     bl_options = {'INTERNAL'}
 
     def execute(self, context: Context) -> set[str]:
-        settings = context.scene.dbu_users_settings
+        settings = get_settings()
 
         settings.parent_map.clear()
         settings.user_map.clear()
@@ -128,7 +131,7 @@ class DBU_OT_UserMapAddAll(Operator):
     bl_options = {'INTERNAL'}
 
     def execute(self, context: Context) -> set[str]:
-        settings = context.scene.dbu_users_settings
+        settings = get_settings()
         parents = settings.parents
         bl_data = ID_TYPES[settings.id_type].collection
 
@@ -153,10 +156,10 @@ class DBU_OT_UserMapRemove(Operator):
     bl_description = "Remove item"
     bl_options = {'INTERNAL'}
 
-    idx: IntProperty()
+    idx: IntProperty()  # type: ignore
 
     def execute(self, context: Context) -> set[str]:
-        settings = context.scene.dbu_users_settings
+        settings = get_settings()
         idx = self.idx
 
         settings.parents.remove(idx)
@@ -172,7 +175,7 @@ class DBU_OT_UserMapRemoveAll(Operator):
     bl_options = {'INTERNAL'}
 
     def execute(self, context: Context) -> set[str]:
-        settings = context.scene.dbu_users_settings
+        settings = get_settings()
 
         settings.parents.clear()
         settings.parent_map.clear()
