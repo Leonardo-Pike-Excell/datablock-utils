f06c72cf042eeec110c7fc8411b439f07666e33e
Leonardo-Pike-Excell <leonardopike.excell@gmail.com>
2024-12-27 17:45:12 +1100

    refactor: improve annotations for go_to_datablock.py

--- source/operators/go_to_datablock.py
+++ source/operators/go_to_datablock.py
@@ -2,22 +2,24 @@
 
 from collections.abc import Collection, Iterator, Sequence
 from itertools import chain
+from typing import cast
 
 import bpy
 from bpy.props import StringProperty
-from bpy.types import ID, Context, Light, Material, Operator, ShaderNodeTree
+from bpy.types import ID, Context, Light, Material, Operator, ShaderNodeTree, SpaceNodeEditor
 
 from ..constants import ID_TYPES
 
 
 def view_selected_delayed(area: bpy.types.Area, region: bpy.types.Region) -> float:
+    assert bpy.context
     with bpy.context.temp_override(area=area, region=region):
         bpy.ops.node.view_selected()
         return 0.0
 
 
 def get_users(subset: Sequence[ID], value_types: set[str]) -> list[ID]:
-    users = bpy.data.user_map(subset=subset, value_types=value_types)
+    users = bpy.data.user_map(subset=subset, value_types=value_types)  # type: ignore
     return list(chain(*users.values()))
 
 
@@ -30,22 +32,23 @@ def get_users_recursive(subset: Sequence[ID], value_types: set[str]) -> Iterator
             yield from get_users_recursive([user], value_types)
 
 
-def get_material_path(
-  users: Sequence[ID],
-  mesh_users: Collection[ID],
+def get_path_to_material(
+  users: Sequence[Material | ShaderNodeTree],
+  obj_data_users: Collection[ID],
   container: ShaderNodeTree | None = None,
 ) -> tuple[Material, ShaderNodeTree | None]:
     try:
         mat = next(
-          u for u in users if isinstance(u, Material) and any(m.user_of_id(u) for m in mesh_users))
+          u for u in users
+          if isinstance(u, Material) and any(m.user_of_id(u) for m in obj_data_users))
         return mat, container
     except StopIteration:
         nested_users = get_users([users[0]], {'MATERIAL', 'NODETREE'})
-        return get_material_path(nested_users, mesh_users, users[0])
+        return get_path_to_material(nested_users, obj_data_users, users[0])  # type: ignore
 
 
-def get_light_path(
-  users: Sequence[ID],
+def get_path_to_light(
+  users: Sequence[Light | ShaderNodeTree],
   container: ShaderNodeTree | None = None,
 ) -> tuple[Light, ShaderNodeTree | None]:
     try:
@@ -53,14 +56,15 @@ def get_light_path(
         return light, container
     except StopIteration:
         nested_users = get_users([users[0]], {'LIGHT', 'NODETREE'})
-        return get_light_path(nested_users, users[0])
+        return get_path_to_light(nested_users, users[0])  # type: ignore
 
 
 def get_geometry_node_group(
-  space: bpy.types.Space,
+  space: SpaceNodeEditor,
   id_data: bpy.types.GeometryNodeTree,
 ) -> bpy.types.GeometryNodeGroup:
-    nodes = [n for n in space.edit_tree.nodes if n.bl_idname == 'GeometryNodeGroup']
+    # Use `isinstance()` over `bl_idname` to satisfy Pyright
+    nodes = [n for n in space.edit_tree.nodes if isinstance(n, bpy.types.GeometryNodeGroup)]
     try:
         node = next(n for n in nodes if n.node_tree == id_data)
     except StopIteration:
@@ -78,10 +82,10 @@ class DBU_OT_GoToDatablock(Operator):
     bl_description = "See where this data-block is used"
     bl_options = {'INTERNAL'}
 
-    id_name: StringProperty()
-    id_type: StringProperty()
-    node_name: StringProperty()
-    settings: StringProperty(default='dbu_users_settings')
+    id_name: StringProperty()  # type: ignore
+    id_type: StringProperty()  # type: ignore
+    node_name: StringProperty()  # type: ignore
+    settings: StringProperty(default='dbu_users_settings')  # type: ignore
 
     def execute(self, context: Context) -> set[str]:
         id_type = self.id_type
@@ -99,6 +103,7 @@ class DBU_OT_GoToDatablock(Operator):
 
         is_mat = id_type == 'MATERIAL'
         is_obj = 'OBJECT' in id_type
+        shr_obj_users = geo_obj_users = subset = None
 
         if id_type in {'MATERIAL', 'SHADER_NODETREE', 'IMAGE'}:
             if not is_mat:
@@ -117,6 +122,7 @@ class DBU_OT_GoToDatablock(Operator):
         else:
             raw_obj_users = list(set(get_users_recursive([id_data], {'OBJECT'})))
 
+        raw_obj_users = cast(list[bpy.types.Object], raw_obj_users)
         if not raw_obj_users:
             self.report({'WARNING'}, "No object users")
             return {'CANCELLED'}
@@ -126,7 +132,7 @@ class DBU_OT_GoToDatablock(Operator):
         node_name = self.node_name
         is_obj_data = ID_TYPES[id_type].is_object_data and not node_name
         view_layer = context.view_layer.objects
-        obj_users: list[bpy.types.Object] = [o for o in raw_obj_users if o.name in view_layer]
+        obj_users = [o for o in raw_obj_users if o.name in view_layer]
 
         settings = getattr(context.scene, self.settings)
         if settings.select_object_users or (is_obj or is_obj_data) or not obj_users:
@@ -162,10 +168,12 @@ class DBU_OT_GoToDatablock(Operator):
         elif not is_geo:
             if geo_obj_users and not shr_obj_users:
                 return {'FINISHED'}
+
             if subset:
                 if not is_mat:
                     users = get_users([id_data], {'MATERIAL', 'NODETREE'})
-                    mat, container = get_material_path(users, [o.data for o in obj_users])
+                    obj_data_users = [o.data for o in obj_users if o.data]
+                    mat, container = get_path_to_material(users, obj_data_users)  # type: ignore
                 else:
                     mat = id_data
 
@@ -176,29 +184,31 @@ class DBU_OT_GoToDatablock(Operator):
                         break
             else:
                 users = get_users([id_data], {'LIGHT', 'NODETREE'})
-                light, container = get_light_path(users)
-                obj = next(o for o in obj_users if o.user_of_id(light))
+                light, container = get_path_to_light(users)  # type: ignore
+                obj = next(o for o in obj_users if o.user_of_id(light))  # type: ignore
         else:
             obj = obj_users[0]
             obj.modifiers.active = next(
               m for m in obj.modifiers
               if (t := getattr(m, 'node_group', None)) and t.contains_tree(id_data))
 
-        view_layer.active = obj
+        view_layer.active = obj  # pyright: ignore [reportUnboundVariable]
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        assert bpy.context
 
         try:
             area = next(
               a for a in bpy.context.window.screen.areas
-              if a.type == 'NODE_EDITOR' and not a.spaces[0].pin)
+              if a.type == 'NODE_EDITOR' and not cast(SpaceNodeEditor, a.spaces[0]).pin)
             region = next(r for r in area.regions if r.type == 'WINDOW')
         except StopIteration:
             return {'FINISHED'}
 
         area.ui_type = 'GeometryNodeTree' if is_geo else 'ShaderNodeTree'
         with bpy.context.temp_override(area=area, region=region):
-            space: bpy.types.SpaceNodeEditor = context.space_data
+            space = cast(SpaceNodeEditor, context.space_data)
 
             if not is_geo:
                 space.shader_type = 'OBJECT'
@@ -220,14 +230,17 @@ class DBU_OT_GoToDatablock(Operator):
             if node_name:
                 node = nodes[node_name]
             elif id_type == 'SHADER_NODETREE':
+                # Use `isinstance()` over `bl_idname` to satisfy Pyright
                 node = next(
-                  n for n in nodes if n.bl_idname == 'ShaderNodeGroup' and n.node_tree == id_data)
+                  n for n in nodes
+                  if isinstance(n, bpy.types.ShaderNodeGroup) and n.node_tree == id_data)
             elif is_geo:
                 node = get_geometry_node_group(space, id_data)
                 nodes = space.edit_tree.nodes  # In case the current tree changed
             else:
                 node = next(
-                  n for n in nodes if n.bl_idname == 'ShaderNodeTexImage' and n.image == id_data)
+                  n for n in nodes
+                  if isinstance(n, bpy.types.ShaderNodeTexImage) and n.image == id_data)
 
             for n in nodes:
                 n.select = n == node
@@ -253,7 +266,7 @@ class DBU_OT_RehideObjectsUsers(Operator):
     bl_description = "Rehide object users that were previously hidden"
     bl_options = {'INTERNAL', 'UNDO'}
 
-    settings: StringProperty()
+    settings: StringProperty()  # type: ignore
 
     def execute(self, context: Context) -> set[str]:
         settings = getattr(context.scene, self.settings)
